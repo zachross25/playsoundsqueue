af3b6d0fbbd8f1659848552a093a957c3d28a3ce
megido_ <megido666@gmail.com>
2019-02-23 17:04:28 +0200

    Updated to Angular 7 version fixed Webpack issue Added material styles developed initial grid template with material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule,
+      MatCardModule,
+      MatButtonModule,
+      MatCheckboxModule,
+      MatGridListModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatCheckboxModule
+
+  ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
